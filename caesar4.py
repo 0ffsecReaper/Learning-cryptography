2c93e10e153375c8b259f7c014342c1ad72bab6c
The 0ff$ec Reaper <108802787+0ffsecReaper@users.noreply.github.com>
2023-10-21 01:50:24 -0700

    Add files via upload

---
+++ caesar4.py
@@ -0,0 +1,14 @@
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+str_in = input("Enter message, like HELLO: ")
+shift = int(input("Shifting value, in numbers: "))
+
+n = len(str_in)
+str_out = ""
+
+for i in range(n):
+  c = str_in[i]
+  loc = alpha.find(c)
+  newloc = (loc + shift)%26
+  str_out += alpha[newloc]
+
+print("Obfuscated version: ", str_out)       
